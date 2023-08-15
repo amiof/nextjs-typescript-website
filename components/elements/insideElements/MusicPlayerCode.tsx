97fc36ada787b8711481c169b2ca017c7e76a575
amiof <arm131313@gmail.com>
2023-08-15 13:06:37 +0330

    add code sample

---
+++ components/elements/insideElements/MusicPlayerCode.tsx
@@ -0,0 +1,14 @@
+export const test =`
+import React from "react";
+import {NextUIProvider} from "@nextui-org/react";
+
+const Application = ({Component, pageProps}) => {
+  return (
+    <NextUIProvider>
+      <main className={isDark ? "dark" : "light"}>
+        <Component {...pageProps} />
+      </main>
+    </NextUIProvider>
+  );
+};
+`
