1c4eee94f67ceda88ef2b2afcb1a65e8b9854cb1
amiof <arm131313@gmail.com>
2023-08-15 22:09:22 +0330

    import data for element white props

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,8 @@ import Head from 'next/head'
 import Mains from "components/main/main"
 import Footer from '@/components/Footer'
 import Element from '@/components/elements/elements'
-
+import { test } from '@/components/elements/insideElements/MusicPlayerCode'
+import { codeSampleButton } from '@/components/elements/insideElements/coustom-buttonCode'
 export default function Home() {
   return (
     <>
@@ -20,9 +21,9 @@ export default function Home() {
       <Mains></Mains>
       <div className='w-full bg-[url("../public/pics/3.png")] bg-cover p-5 bg-blur-2xl'>
         <div className='flex-col '>
-          <Element ></Element>
-          <Element></Element>
-          <Element></Element>
+          <Element code={codeSampleButton} title='code sample button'></Element>
+          <Element code={test} title='musice player'></Element>
+          <Element code={test} title='music player'></Element>
         </div>
       </div>
       <Footer></Footer>
