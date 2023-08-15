59546529332bc62dac99e6f68815e7a0a16bcd3a
amiof <arm131313@gmail.com>
2023-08-15 13:29:46 +0330

    add arrow icon for open and close

--- components/elements/elements.tsx
+++ components/elements/elements.tsx
@@ -10,13 +10,21 @@ function Element({ }: Props) {
   const clickHandler = () => {
     setOpenCollaps(!openCollaps)
   }
+  //google font icon for icon used https://fonts.google.com/icons?icon.platform=web&icon.set=Material+Symbols
+
   return (
     <div className=' w-2/3 bg-gray-900 bg-opacity-20 text-white mx-auto mt-5  rounded-xl backdrop-blur-lg border border-gray-700 divide-y divide-dash divide-gray-800   '>
       <div className='flex justify-between p-5 ' onClick={clickHandler}>
         <p className='ml-20' >shipped</p>
-        <span className="material-symbols-outlined">
-          expand_less
-        </span>
+        {openCollaps ?
+          <span className="material-symbols-outlined">
+            expand_less
+          </span>
+          :
+          <span className="material-symbols-outlined" >
+            expand_more
+          </span>
+        }
       </div>
       <div className={!openCollaps ? "hidden" : "w-full h-[400px] flex "} >
         <div className='w-3/5 border border-yellow-300 '>
